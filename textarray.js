fe89b65b84c465f7bdf16106be4089f2ad81c41b
Cameron Carroll <ckcarroll4@gmail.com>
2013-01-09 05:47:48 -0800

    Added express config and routes to main file

--- textarray.js
+++ textarray.js
@@ -1,12 +1,32 @@
-var express = require('express');
+'use strict';
+
+var express = require('express'),
+    db = require('./lib/db'),
+    arylib = require('./lib/arylib');
 
 var app = express(express.logger());
 
-app.get('/', function(req, res) {
-  res.send('hello heroku');
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'txtarysecretsauce'}));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname + '/public'));
 });
 
+app.set('view options', {
+  layout: false
+});
+
+app.get('/', aryroutes.getIndex);
+app.get('/login', aryroutes.login);
+app.get('/signup', aryroutes.signup);
+app.get('/job', aryroutes.job);
+app.get('/api/user/:username', aryroutes.getUser);
+
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+db.open(function() {
+  console.log("Database opened successfully. Listening on port: " + port);
+  app.listen(port);
 });
